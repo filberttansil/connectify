5ae5df97118edbdd0517ae9c1a0f9c9c850209d3
filberttansil <filberttansil1212@gmail.com>
2023-07-06 15:59:54 +0700

    fiturAdd

--- routers/router.js
+++ routers/router.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/controller");
 
+//home
 router.get("/home", Controller.renderHome);
-router.get("/addPost", Controller.addPost);
-
+//render add post form
+router.get("/addPost", Controller.renderPostForm);
+//add post to database
+router.post("/addPost", Controller.handleAddPost);
+//render add Tags form
+router.get("/addTags", Controller.renderTagsForm);
+//handle add Tag
+router.post("/addTags", Controller.handleAddTag);
 module.exports = router;
